3717dcfe4e1338c780476913b0dc300c3bada1f6
tomotake nagao <nagao@interspace.ne.jp>
2018-08-01 11:27:37 +0900

    no message

---
+++ src/main/java/com/example/HelloSpringbootController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringbootController {
+
+	private final static Logger logger = LoggerFactory.getLogger(HelloSpringbootController.class);
+
+	@RequestMapping({ "/" })
+	public String index() {
+
+		logger.info("Hello");
+		return "Hello Springboot!";
+	}
+}
